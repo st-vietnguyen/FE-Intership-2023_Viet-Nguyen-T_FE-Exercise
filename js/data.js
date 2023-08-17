9309ff453c4bc97d3ee4652f1007e038a2368b9a
st-vietnguyen <viet.nguyen@supremetech.vn>
2023-08-17 10:52:26 +0700

    create cart page

--- js/data.js
+++ js/data.js
@@ -1,35 +1,40 @@
 import Product from './product.js';
 
-const data = [
-  new Product(
-    0,
-    'T-shirt Summer Vibes',
-    199.99,
-    '/assets/images/product-1.png',
-    30,
-    14
-  ),
-  new Product(
-    1,
-    'T-shirt Summer Vibes',
-    199.99,
-    '/assets/images/product-2.png',
-    0
-  ),
-  new Product(
-    2,
-    'T-shirt Summer Vibes',
-    199.99,
-    '/assets/images/product-3.png',
-    20
-  ),
-  new Product(
-    3,
-    'T-shirt Summer Vibes',
-    199.99,
-    '/assets/images/product-4.png',
-    10
-  ),
+const productList = [
+  {
+    id: 0,
+    name: 'T-shirt Summer Vibes',
+    price: 199.99,
+    image: '../assets/images/product-1.png',
+    discount: 30,
+    quantity: 14,
+  },
+  {
+    id: 1,
+    name: 'T-shirt Summer Vibes',
+    price: 199.99,
+    image: '../assets/images/product-2.png',
+    discount: 0,
+    quantity: 11,
+  },
+  {
+    id: 2,
+    name: 'T-shirt Summer Vibes',
+    price: 199.99,
+    image: '../assets/images/product-3.png',
+    discount: 20,
+    quantity: 6,
+  },
+  {
+    id: 3,
+    name: 'T-shirt Summer Vibes',
+    price: 199.99,
+    image: '../assets/images/product-4.png',
+    discount: 10,
+    quantity: 10,
+  },
 ];
+
+const data = productList.map(product => new Product(product));
 
 export default data;
